84ce79ca0239d2f52466aa15b63eae543db8365a
Ann S <404region@gmail.com>
2023-10-14 16:37:59 +0300

    PP_2_2_1_5

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -5,6 +5,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
+
+    //На свете есть океан , на океане остров , на острове дерево , на дереве заяц ,
+    // в зайце утка , в утке яйцо , в яйце иголка , смерть Кощея на игле :(
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
